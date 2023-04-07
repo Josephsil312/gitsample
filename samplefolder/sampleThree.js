51bede16b1283949406ea7ff2e775dfab3c1fd08
abhinav19112021 <abhinav.parashar.dorbit@doodle.com>
2023-04-07 21:28:24 +0530

    fs from featthree

---
+++ samplefolder/sampleThree.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const sampleThree = () => {
+  return (
+    <View>
+      <Text>sampleThree</Text>
+    </View>
+  )
+}
+
+export default sampleThree
+
+const styles = StyleSheet.create({})
